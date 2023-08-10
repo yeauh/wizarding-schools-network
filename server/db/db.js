fb49ca6a563d5a87dd06c614e1dee5042e7b2834
alexYea <alexanderterekhov1@gmail.com>
2023-08-09 22:49:41 -0400

    commit

--- server/db/db.js
+++ server/db/db.js
@@ -13,5 +13,76 @@ console.log(chalk.yellow("Opening database connection"));
 const db = new Sequelize(`postgres://localhost:5432/${pkg.name}`, {
   logging: false, // so we don't see all the SQL queries getting made
 });
+ /*
+  wizarding-shools model:
+  name - not empty or null
+  imageUrl - with a default value
+  location - not empty or null
+  description - extremely large text
+ */
+
+const wizardingSchools = db.define("wizardingSchools", {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue: "google.com",
+  },
+  location: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.STRING,
+  },
+});
+
+
+/* 
+  firstName - not empty or null
+  lastName - not empty or null
+  email - not empty or null; must be a valid email
+  imageUrl - with a default value
+  magicalAbilityScore - decimal between 0 and 10.0
+  Students may be associated with at most one wizarding school.
+  Likewise, wizarding schools may be associated with many students
+*/
+const student = db.define("student", {
+  firstname: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: {
+        msg: "Must be a valid email address",
+      }
+    }
+  },
+  magicalAbilityScore: {
+    type: Sequelize.FLOAT,
+    validate: {
+      max: 10.0,
+      min: 0.0,
+    }
+  }
+});
+
+/*
+  Students may be associated with at most one wizarding school.
+  Likewise, wizarding schools may be associated with many students
+ */
+
+wizardingSchools.hasMany(student);
+student.belongsTo(wizardingSchools);
 
 module.exports = db;
